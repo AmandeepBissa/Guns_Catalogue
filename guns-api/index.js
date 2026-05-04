6dad01411b1a267b0cda206fc94d63ea1cfb9673
Amandeep Bissa <amandeepbissa@gmail.com>
2026-05-04 17:49:07 +0530

    Made views folder

--- guns-api/index.js
+++ guns-api/index.js
@@ -7,14 +7,14 @@ app.use(cors());
 app.use(express.json());
 
 let guns = [
-  { id: 1, category: "Pistols", name: "Colt M1911", feature: "Semi-Automatic", price: 100, link: "Colt.html" },
-  { id: 2, category: "Pistols", name: "Ruger GP100", feature: "Revolver", price: 100, link: "Ruger.html" },
-  { id: 3, category: "Assault Rifles", name: "AK-47", feature: "7.62x39mm", price: 200, link: "AK47.html" },
-  { id: 4, category: "Assault Rifles", name: "M4 Carbine", feature: "5.56mm", price: 200, link: "M4.html" },
-  { id: 5, category: "Shotguns", name: "Stevens 301", feature: "Single-Barrel", price: 300, link: "Stevens.html" },
-  { id: 6, category: "Shotguns", name: "Stoeger Coach Gun", feature: "Double-Barrel", price: 300, link: "Stoeger.html" },
-  { id: 7, category: "Snipers", name: "M24 SWS", feature: "Military grade", price: 400, link: "M24.html" },
-  { id: 8, category: "Snipers", name: "AX308", feature: "Police grade", price: 400, link: "AX308.html" }
+  { id: 1, category: "Pistols", name: "Colt M1911", feature: "Semi-Automatic", price: 100, link: "views/Colt.html" },
+  { id: 2, category: "Pistols", name: "Ruger GP100", feature: "Revolver", price: 100, link: "views/Ruger.html" },
+  { id: 3, category: "Assault Rifles", name: "AK-47", feature: "7.62x39mm", price: 200, link: "views/AK47.html" },
+  { id: 4, category: "Assault Rifles", name: "M4 Carbine", feature: "5.56mm", price: 200, link: "views/M4.html" },
+  { id: 5, category: "Shotguns", name: "Stevens 301", feature: "Single-Barrel", price: 300, link: "views/Stevens.html" },
+  { id: 6, category: "Shotguns", name: "Stoeger Coach Gun", feature: "Double-Barrel", price: 300, link: "views/Stoeger.html" },
+  { id: 7, category: "Snipers", name: "M24 SWS", feature: "Military grade", price: 400, link: "views/M24.html" },
+  { id: 8, category: "Snipers", name: "AX308", feature: "Police grade", price: 400, link: "views/AX308.html" }
 ];
 
 // TEST
